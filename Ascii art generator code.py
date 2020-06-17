c4e1735e450886c14e4deb8824f87ee3e4825e30
trisha <farjana186@gmail.com>
2020-06-17 20:56:59 +0600

    second comment

---
+++ Ascii art generator code.py
@@ -0,0 +1,5 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> f = open("ASCII ART.txt", "r")
+print(f.read())
+f.close()
